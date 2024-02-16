c092fbe85a59e592e8878f04200ccd74f03ad927
codebreaker343 <deore.venkatesh@gmail.com>
2024-02-17 03:14:54 +0530

    Setting up basic boilerplate

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
+import * as express from 'express';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(express.json({ limit: '50mb' }));
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableShutdownHooks();
+
+  const config = new DocumentBuilder()
+    .setTitle('Notifier')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+
+bootstrap();
